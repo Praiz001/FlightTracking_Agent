17c3df15795b73985af0128fd12ce0c41c793c24
Praiz001 <oshilimpraisek@gmail.com>
2025-11-03 00:07:28 +0100

    feat: remove weather agent

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,9 +2,7 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
 import { flightTrackerWorkflow } from './workflows/flight-tracker-workflow';
-import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
 import { agents } from './agents';
 import { a2aAgentRoute } from './routes/a2a-agent-route';
 
@@ -12,12 +10,12 @@ import { a2aAgentRoute } from './routes/a2a-agent-route';
 
 
 
-const { flightTrackerAgent, weatherAgent } = agents;
+const { flightTrackerAgent } = agents;
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow, flightTrackerWorkflow },
-  agents: { flightTrackerAgent, weatherAgent },
-  scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
+  workflows: { flightTrackerWorkflow },
+  agents: { flightTrackerAgent },
+  // scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
   storage: new LibSQLStore({
     // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
@@ -27,7 +25,6 @@ export const mastra = new Mastra({
     level: 'info',
   }),
   telemetry: {
-    // Telemetry is deprecated and will be removed in the Nov 4th release
     enabled: false,
   },
   observability: {
